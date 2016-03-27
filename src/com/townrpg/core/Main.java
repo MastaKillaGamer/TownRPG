86a2b9d6775deae6c1c7a6f828392ad27bf5eb8d
MastaKillaGamer <kalebtbacon@gmail.com>
2016-03-27 19:47:01 -0400

    Initial commit

---
+++ src/com/townrpg/core/Main.java
@@ -0,0 +1,9 @@
+package com.townrpg.core;
+
+import com.townrpg.core.display.Display;
+
+public class Main {
+    public static void main(String[] args) {
+        new Display("Title", 300,300);
+    }
+}
